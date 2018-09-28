9b0c7eab6ed205b048d0936e0f474ec59e959aa0
TeraNovaLP <TeraNovaLPs@gmail.com>
2018-09-28 20:30:16 +0200

    Add Service System

---
+++ src/main/java/eu/playperium/spongecore/service/ServiceManager.java
@@ -0,0 +1,27 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package eu.playperium.spongecore.service;
+
+import java.util.ArrayList;
+
+public class ServiceManager {
+
+    private ArrayList<InternalService> internalServices = new ArrayList<>();
+    
+    public ServiceManager() {
+        this.loadServices();
+    }
+
+    public void registerInternalService(InternalService service) {
+        internalServices.add(service);
+    }
+    
+    private void loadServices() {
+        for (InternalService service: internalServices) {
+            service.execute();
+        }
+    }
+}
